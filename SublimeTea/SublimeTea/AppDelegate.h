e214fdb99eecdf61c80c9f897381b4b2dcc7a10a
Arpit Mishra <Arpit.Mishra@Symphonyteleca.com>
2016-02-24 17:01:11 +0530

    Project creation

---
+++ SublimeTea/SublimeTea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SublimeTea
+//
+//  Created by Arpit Mishra on 24/02/16.
+//  Copyright © 2016 Arpit Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SublimeTea/SublimeTea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SublimeTea
+//
+//  Created by Arpit Mishra on 24/02/16.
+//  Copyright © 2016 Arpit Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
