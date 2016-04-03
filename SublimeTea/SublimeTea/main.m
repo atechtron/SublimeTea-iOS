844b4ac9d67cb9c64b4ce0fdb3a16c465b3a1f3c
Tiffany Parker <tiffany.parker@marlabs.com>
2016-04-03 18:58:24 +0530

    Issue fix.

--- SublimeTea/SublimeTea/main.m
+++ SublimeTea/SublimeTea/main.m
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "STAppDelegate.h"
+#import "TIMERUIApplication.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+        return UIApplicationMain(argc, argv, NSStringFromClass([TIMERUIApplication class]), NSStringFromClass([STAppDelegate class]));
     }
 }
+
