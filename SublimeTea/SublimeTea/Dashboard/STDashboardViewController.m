1eceb77c3ab633496df6009971c6a42c60c68362
Tiffany Parker <tiffany.parker@marlabs.com>
2016-04-09 11:19:51 +0530

    issue fix.

--- SublimeTea/SublimeTea/Dashboard/STDashboardViewController.m
+++ SublimeTea/SublimeTea/Dashboard/STDashboardViewController.m
@@ -74,7 +74,7 @@
         {
             UIView *uiview = [cell.contentView viewWithTag:121333];
             if (!uiview) {
-                _slideshow = [[KASlideShow alloc] initWithFrame:CGRectMake(0, 0, collectionView.frame.size.width, cell.contentView.frame.size.height/2 + 20)];
+                _slideshow = [[KASlideShow alloc] initWithFrame:CGRectMake(0, 0, collectionView.frame.size.width, cell.contentView.frame.size.height-60)];
                 _slideshow.delegate = self;
                 _slideshow.tag = 121333;
                 [_slideshow setDelay:3]; // Delay between transitions
